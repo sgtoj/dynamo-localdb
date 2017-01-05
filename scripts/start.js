cfc3edc036f8931129dd39916bd02b76d9db6ed5
Brian Ojeda <bojeda@connectwise.com>
2017-01-05 10:11:49 -0500

    updated the scripts

--- scripts/start.js
+++ scripts/start.js
@@ -1,9 +1,4 @@
+"use strict";
 let LocalDB = require("../app/main");
-
-let config = {
-    "region": "us-east-1",
-    "endpoint": "http://localhost:8000"
-}
-
-let db = new LocalDB.LocalStore(config);
+let db = new LocalDB.LocalStore();
 db.start();
